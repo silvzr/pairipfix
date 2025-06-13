7a51e715238fa8fa0021c6617ceb17a53493dfed
silvzr <117378129+silvzr@users.noreply.github.com>
2025-06-13 14:56:42 +0200

    test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,12 +13,14 @@ android {
         versionCode = 1
         versionName = "1.0"
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        // Riduce le dimensioni mantenendo solo densità essenziali
+        resConfigs("en", "xxhdpi")
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
@@ -29,14 +31,42 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
+    // Ottimizzazioni per le dimensioni
+    packagingOptions {
+        resources {
+            excludes += listOf(
+                "META-INF/DEPENDENCIES",
+                "META-INF/LICENSE",
+                "META-INF/LICENSE.txt",
+                "META-INF/NOTICE",
+                "META-INF/NOTICE.txt",
+                "META-INF/*.RSA",
+                "META-INF/*.SF",
+                "META-INF/*.DSA",
+                "DebugProbesKt.bin",
+                "kotlin-tooling-metadata.json"
+            )
+        }
+        jniLibs {
+            useLegacyPackaging = false
+        }
+    }
+
+    bundle {
+        density {
+            enableSplit = false
+        }
+        abi {
+            enableSplit = false
+        }
+        language {
+            enableSplit = false
+        }
+    }
 }
 
 dependencies {
-
-    implementation(libs.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.ext.junit)
-    androidTestImplementation(libs.espresso.core)
+    // Rimosso material design e appcompat - non necessari per un modulo Xposed
     compileOnly("de.robv.android.xposed:api:82")
 }
--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -1,21 +1,14 @@
-# Add project specific ProGuard rules here.
-# You can control the set of applied configuration files using the
-# proguardFiles setting in build.gradle.
-#
-# For more details, see
-#   http://developer.android.com/guide/developing/tools/proguard.html
+# Regole ProGuard per ottimizzare un modulo Xposed
+-dontobfuscate
+-keepattributes *Annotation*
+-keep class de.robv.android.xposed.** { *; }
+-keep class com.android.pairipfix.mainHook { *; }
 
-# If your project uses WebView with JS, uncomment the following
-# and specify the fully qualified class name to the JavaScript interface
-# class:
-#-keepclassmembers class fqcn.of.javascript.interface.for.webview {
-#   public *;
-#}
+# Rimuovi tutto ciò che non è necessario
+-dontwarn **
+-dontnote **
 
-# Uncomment this to preserve the line number information for
-# debugging stack traces.
-#-keepattributes SourceFile,LineNumberTable
-
-# If you keep the line number information, uncomment this to
-# hide the original source file name.
-#-renamesourcefileattribute SourceFile
+# Ottimizzazioni aggressive
+-optimizations !code/simplification/arithmetic,!field/*,!class/merging/*,!code/allocation/variable
+-allowaccessmodification
+-repackageclasses ''
